ba56c5472439d30cfc6198c2853fff344617dfba
NguyenVu04 <nguyenvu04.work@gmail.com>
2026-05-16 11:12:30 +0700

    Update urls.py

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("me", views.MeView.as_view(), name="users-me"),
+    path("users/me", views.MeView.as_view(), name="users-me"),
 ]
